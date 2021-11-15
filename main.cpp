042ec5e1c8920c1c541789de867fa6e52ef5b9df
Evita Overtoom <eovertoo@f1r6s4.codam.nl>
2021-11-15 17:56:46 +0100

    added own puzzle generator

--- main.cpp
+++ main.cpp
@@ -3,16 +3,42 @@
 #include "Puzzle.hpp"
 #include <fstream>
 #include <sstream>
+#include <ctime>
+#include <cstdlib>
 
-std::vector<int>&   createPuzzle(std::vector<int> &board) {
-    // int bla[25] = { 17, 9, 24, 12, 15, 14, 10, 4, 0, 11, 7, 6, 1, 8, 22, 3, 18, 19, 23, 16, 20, 13, 5, 2, 21 };
-    // int bla[9] = { 8, 7, 3, 6, 1, 5, 0, 2, 4 };
-    int bla[16] = { 2, 14, 10, 11, 0, 9, 4, 15, 5, 1, 13, 7, 3, 6, 12, 8 };
+int     getUserInput(){
+    std::string input;
+    std::cout << "give the size of a puzzle\n";
+    std::cin >> input;
 
-    for (int i = 0; i < 16; i++) {
-        board[i] = bla[i];
+    for (int i = 0; i < std::strlen(input.c_str()); i++)
+    {
+        if (!isdigit(input[i]))
+            throw std::runtime_error("");
     }
-    return board;
+    int size = stoi(input);
+    if (size <= 1)
+        throw std::runtime_error("");
+    return size;
+}
+
+Node*   createPuzzle(){
+    srand(time(NULL));
+    int size = getUserInput();
+    std::vector<int> grid(size*size, -1);
+    for (int i = 0; i < size*size; i++){
+        while (1){
+            int tmp = rand() % (size * size);
+            if (grid[tmp] == -1){
+                grid[tmp] = i;
+                break;
+            }
+        }
+    }
+    Node *ret = new Node(size);
+    ret->setPuzzle(grid);
+    ret->print();
+    return ret;
 }
 
 void    print(Node &tmp, int moves){
@@ -96,63 +122,24 @@ void    aStarAlgo(Node *start){
 int     main(int argc, char **argv){
     Node *start;
     if (argc != 2){
-        std::cout << "oepsieee\n";
-        return 0;
+        try{
+            start = createPuzzle();
+        }
+        catch(std::exception &e){
+            std::cout << "Invalid size\n";
+            return 0;
+        }// std::cout << "oepsieee\n";
     }
-    try{
-        start = readfile(argv[1]);
+    else{
+        try{
+            start = readfile(argv[1]);
         // start->print();
-    }
-    catch(std::exception &e){
-        std::cout << "Invald input format\n";
-        return 0;
+        }
+        catch(std::exception &e){
+            std::cout << "Invald input format\n";
+            return 0;
+        }
     }
     aStarAlgo(start);
     return 0;
 }
-
-
-
-
-
-
-
-
-
-// int     main(void) {
-
-//     Node    start(4);
-//     std::vector<int> board(16, 0);
-//     int i = 0;
-//     start.setPuzzle(createPuzzle(board));
-//     // start.setHash(start.getPuzzle());
-//     // std::cout << "aaaa  " << start.getHash() << std::endl;
-//     Puzzle  puzzle(4);
-//     std::deque<Node*> bla;
-
-//     puzzle.getOpenList().push(&start);
-//     puzzle.getClosedList().insert(std::make_pair(start.getHash(), 0));
-//     while (!(puzzle.getOpenList().empty()) && i < 5) {
-//         Node *tmp = (puzzle.getOpenList().top());
-//         // if (i >= 0){
-//         //     std::cout << "cur\n";
-//         //     tmp->print();
-//         //     std::cout << tmp->getH() << std::endl;
-//         // }
-//         if (tmp->getH() == 0 && tmp->getG() != 0){
-//             print(*tmp, 0);
-//             std::cout << puzzle.getOpenList().size() << std::endl;
-//             std::cout << puzzle.getClosedList().size() << std::endl;
-//             std::cout << i << std::endl;
-//             break;
-//         }
-//         puzzle.getOpenList().pop();
-//         puzzle.addToList(*tmp);
-//         bla.push_back(tmp);
-//         // i++;
-//     }
-//     bla.erase(bla.begin(), bla.end());
-//     std::cout << "aaa\n";
-//     // while(1);
-// }
-
